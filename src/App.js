89e191c62901c00b94151b46ac43c9da0a0fcda2
hexhad <hexhad@gmail.com>
2023-04-12 20:34:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import { db } from "./utils/firebase.js";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [seriesSet, setSeriesSet] = useState();
+
+  async function call() {
+    const seriesCollection = collection(db, "series");
+    const data = await getDocs(seriesCollection);
+    let struct = data.docs.map((d) => ({ ...d.data(), id_fire: d.id }));
+    setSeriesSet(struct);
+  }
+
+  useEffect(() => {
+    call();
+  }, []);
+
+  const renderList = useMemo(() => {
+    return (
+      <div>
+        {!!seriesSet?.length &&
+          Object?.values(seriesSet).map((e) => (
+            <div key={e.id}>
+              <p>{e.permalink}</p>
+            </div>
+          ))}
+      </div>
+    );
+  }, [seriesSet]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{!!seriesSet?.length && renderList}</div>
     </div>
   );
 }
