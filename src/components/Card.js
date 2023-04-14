940d156b5849d4faf370e4ee4403a65c1b932b9e
hexhad <hexhad@gmail.com>
2023-04-14 23:14:37 +0530

    latest changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,12 +11,21 @@ export default function Card(data) {
     status,
     description,
     pictures,
+    episodes,
+    genres,
   } = data;
+
+  const ratingComp = (stars) => `★★★★★☆☆☆☆☆`.slice(5 - stars, 10 - stars);
+  const formatter = new Intl.ListFormat("en", {
+    style: "long",
+    type: "conjunction",
+  });
+
+  console.log(data);
   return (
     <div key={id} className="card">
-      {/* <div><img src={pictures[0]} alt={name} className="bg"/></div> */}
       <div className="card-container">
-        <div className={`card-tag ${status=='Running' && 'runnning'}`}>
+        <div className={`card-tag ${status === "Running" && "runnning"}`}>
           <p>{status}</p>
         </div>
         <div className="card-image-wrapper">
@@ -24,10 +33,21 @@ export default function Card(data) {
         </div>
         <div className="card-data-wrapper">
           <p className="card-name">{name}</p>
-          <p className="card-rating">{rating}</p>
-          <p className="card-status">{status}</p>
-          <p className="card-desc">{description}</p>
+          <p className="card-genres">{formatter.format(genres)}</p>
+          <p className="card-rating">{ratingComp(rating / 2)}</p>
+          {/* <p className="card-status">{status}</p> */}
+          <span className="card-desc">
+            {description.replaceAll(/<.*>|<\/.*>/g, "")}
+          </span>
+          <p className="card-episodes">{`Episodes : ${episodes.length}`}</p>
           <p className="card-network">{network}</p>
+          <div className="sub-image-set">
+            {pictures?.map((img, index) => index < 4 && <img src={img} />)}
+          </div>
+          {/* <div className="card-bottom">
+            <div className="red-fancy"></div>
+            <div className="blue-fancy"></div>
+          </div> */}
         </div>
       </div>
     </div>
--- src/routes/MainScreen.js
+++ src/routes/MainScreen.js
@@ -37,6 +37,9 @@ export default function MainScreen() {
     showArrows={false}
     autoPlay
     infiniteLoop
+    stopOnHover
+    swipeable
+    showThumbs
       >{!!seriesSet?.length && renderList}</Carousel>
     </div>
   );
